79b97951162f43c911947a0c56b43d52145aad4a
Renaud Merle <rmerle@cockpit-itsm.com>
2017-10-05 21:16:05 +0200

    Update jest-vue to v0.7.0 and other packages to latest

--- App.vue
+++ App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>Test<sub /></div>
+    <div>Test<sub-comp/></div>
 </template>
 
 <script lang="ts">
@@ -8,7 +8,7 @@ import Sub from "./Sub.vue";
 export default {
     name: "Component",
     components: {
-        Sub
+        "sub-comp": Sub
     }
 }
 </script>
